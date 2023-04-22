281974e3184bccaa882bd5e79237f52c06f07a24
VictorRms03 <victorrms03@gmail.com>
2023-04-22 01:49:18 -0300

    Pequenas Mudanças no código e adição do Botão "O que é Lançamento Obliquo"

--- src/main/java/com/mycompany/lancamentopreciso/App.java
+++ src/main/java/com/mycompany/lancamentopreciso/App.java
@@ -2,7 +2,6 @@ package com.mycompany.lancamentopreciso;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -25,15 +24,15 @@ public class App extends Application {
     public void start(Stage stage) throws IOException {
         
         // Importa e configura visualização da logo
-        InputStream inputLogo = new FileInputStream("logo.PNG");
-        Image logo = new Image(inputLogo);
-        ImageView logoView = new ImageView();
-        logoView.setImage(logo);
+        Image logo = new Image(new FileInputStream("logo.PNG"));
+        ImageView logoView = new ImageView(logo);
         logoView.setX(185);
         logoView.setY(30);
         
-        // Texto "O QUE DESEJA CALCULAR"
-        Rectangle blocoTextoOQueDesejaCalcular = new Rectangle(120, 200, 300, 30);
+        // Configuração Texto "O QUE DESEJA CALCULAR"
+        Rectangle blocoTextoOQueDesejaCalcular = new Rectangle(300, 30);
+        blocoTextoOQueDesejaCalcular.setX(120);
+        blocoTextoOQueDesejaCalcular.setY(200);
         blocoTextoOQueDesejaCalcular.setArcHeight(10);
         blocoTextoOQueDesejaCalcular.setArcWidth(10);
         blocoTextoOQueDesejaCalcular.setFill(Color.WHITE);
@@ -47,8 +46,10 @@ public class App extends Application {
         
         Group abaOQueDesejaCalcular = new Group(blocoTextoOQueDesejaCalcular, textoOQueDesejaCalcular);
         
-        // Texto "Movimento Vertical"
-        Rectangle blocoTextoMovimentoVertical = new Rectangle(40, 250, 180, 40);
+        // Configuração Texto "Movimento Vertical"
+        Rectangle blocoTextoMovimentoVertical = new Rectangle(180, 40);
+        blocoTextoMovimentoVertical.setX(40);
+        blocoTextoMovimentoVertical.setY(250);
         blocoTextoMovimentoVertical.setArcHeight(10);
         blocoTextoMovimentoVertical.setArcWidth(10);
         blocoTextoMovimentoVertical.setFill(Color.TURQUOISE);
@@ -61,8 +62,10 @@ public class App extends Application {
         
         Group abaMovimentoVertical = new Group(blocoTextoMovimentoVertical, textoMovimentoVertical);
         
-        // Texto "Movimento Horizontal"
-        Rectangle blocoTextoMovimentoHorizontal = new Rectangle(290, 250, 180, 40);
+        // Configuração Texto "Movimento Horizontal"
+        Rectangle blocoTextoMovimentoHorizontal = new Rectangle(180, 40);
+        blocoTextoMovimentoHorizontal.setX(290);
+        blocoTextoMovimentoHorizontal.setY(250);
         blocoTextoMovimentoHorizontal.setArcHeight(10);
         blocoTextoMovimentoHorizontal.setArcWidth(10);
         blocoTextoMovimentoHorizontal.setFill(Color.TURQUOISE);
@@ -75,50 +78,55 @@ public class App extends Application {
         
         Group abaMovimentoHorizontal = new Group(blocoTextoMovimentoHorizontal, textoMovimentoHorizontal);
         
-        // Botão "Posição Vertical em Função do Tempo"
+        // Configuração Botão "Posição Vertical em Função do Tempo"
         Button botaoPosicaoVerticalEmFuncaoDoTempo = new Button("Posiçao Vertical em Função do Tempo");
         botaoPosicaoVerticalEmFuncaoDoTempo.setLayoutX(10);
         botaoPosicaoVerticalEmFuncaoDoTempo.setLayoutY(310);
         botaoPosicaoVerticalEmFuncaoDoTempo.setMinWidth(235);
         
-        // Botão "Velocidade Vertical em Função do Tempo"
+        // Configuração Botão "Velocidade Vertical em Função do Tempo"
         Button botaoVelocidadeVerticalEmFuncaoDoTempo = new Button("Velocidade Vertical em Função do Tempo");
         botaoVelocidadeVerticalEmFuncaoDoTempo.setLayoutX(10);
         botaoVelocidadeVerticalEmFuncaoDoTempo.setLayoutY(360);
         botaoVelocidadeVerticalEmFuncaoDoTempo.setMinWidth(235);
         
-        // Botão "Tempo de Subida"
+        // Configuração Botão "Tempo de Subida"
         Button botaoTempoDeSubida = new Button("Tempo de Subida");
         botaoTempoDeSubida.setLayoutX(10);
         botaoTempoDeSubida.setLayoutY(410);
         botaoTempoDeSubida.setMinWidth(235);
         
-        // Botão "Altura Máxima"
+        // Configuração Botão "Altura Máxima"
         Button botaoAlturaMaxima = new Button("Altura Maxima");
         botaoAlturaMaxima.setLayoutX(10);
         botaoAlturaMaxima.setLayoutY(460);
         botaoAlturaMaxima.setMinWidth(235);
 
-        // Botão "Posição Horizontal em Função do Tempo"
+        // Configuração Botão "Posição Horizontal em Função do Tempo"
         Button botaoPosicaoHorizontalEmFuncaoDoTempo = new Button("Posiçao Horizontal em Função do Tempo");
         botaoPosicaoHorizontalEmFuncaoDoTempo.setLayoutX(270);
         botaoPosicaoHorizontalEmFuncaoDoTempo.setLayoutY(310);
         botaoPosicaoHorizontalEmFuncaoDoTempo.setMinWidth(235);
         
-        // Botão "Alcance Horizontal"
+        // Configuração Botão "Alcance Horizontal"
         Button botaoAlcanceHorizontal = new Button("Alcance Horizontal");
         botaoAlcanceHorizontal.setLayoutX(270);
         botaoAlcanceHorizontal.setLayoutY(360);
         botaoAlcanceHorizontal.setMinWidth(235);
-
         
         // Grupo para aba Seleção de Calculos
         Group abaSeleçãoDeCalculos = new Group(abaOQueDesejaCalcular, abaMovimentoVertical, abaMovimentoHorizontal, 
                 botaoPosicaoVerticalEmFuncaoDoTempo, botaoVelocidadeVerticalEmFuncaoDoTempo, botaoTempoDeSubida, 
                 botaoAlturaMaxima, botaoPosicaoHorizontalEmFuncaoDoTempo, botaoAlcanceHorizontal);
         
+        // Configuração Botão "Entenda o Que é Lançamento Obliquo"
+        Button botaoEntendaOQueELancamentoObliquo = new Button("Entenda O Que é Lançamento Obliquo");
+        botaoEntendaOQueELancamentoObliquo.setLayoutX(550);
+        botaoEntendaOQueELancamentoObliquo.setLayoutY(30);
+        botaoEntendaOQueELancamentoObliquo.setMinSize(200, 40);
+        
         // grupo tela inteira
-        Group telaInteira = new Group(logoView, abaSeleçãoDeCalculos);
+        Group telaInteira = new Group(logoView, abaSeleçãoDeCalculos, botaoEntendaOQueELancamentoObliquo);
         
         // configuração da cena
         var scene = new Scene(telaInteira,1200, 600);
